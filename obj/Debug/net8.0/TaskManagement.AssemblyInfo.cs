af8d439f9f11e2255a893ce69213a9316951283e
Nurdan Kurcak <nurdank@uia.no>
2024-12-12 02:39:11 +0100

    design, friends, etc..

--- obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9575bec2603bf73d9f9f2e2e1272f7af0513da8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd0b552fc0f434eeba603544ab6d2ea814d820f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
